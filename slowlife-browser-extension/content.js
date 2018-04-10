40a3595b158481ab69984af83a293924b179b666
juhanurmi <juha.nurmi@ahmia.fi>
2018-04-10 08:43:46 +0300

    A word list based harmful content detection. Then the wait time is 100 seconds.

--- slowlife-browser-extension/content.js
+++ slowlife-browser-extension/content.js
@@ -1,10 +1,53 @@
 // The extension is running this script on every page.
 // You can add any JavaScript you want here.
 
+function find_harmfull_content() {
+
+    // These are just examples of the most useless internet distraction
+    var finnish_harmful_keywords = ["uutiset", "sanomat", "lehti", "otsikot", "suosituimmat"];
+    var news = ["headlines", "secrets", "gossip", "pop culture", "celebr", "scandal"];
+    var funny = ["funny", "cats", "trump", "stupid", "epic", "failure", "owned", "comics", "idiot"];
+    var nonsense = ["viral", "these people", "they say", "triggered", "warrior", "memes", "tweets"];
+    var adult = ["porn", "penis", "pussy", "fuck", " sex ", "naked", "blowjob", "homemade"];
+    var useless = ["entertainment", "commentary", "amazing", "most popular", "current events"];
+
+    var stopwords = finnish_harmful_keywords;
+    var stopwords = stopwords.concat(news);
+    var stopwords = stopwords.concat(funny);
+    var stopwords = stopwords.concat(nonsense);
+    var stopwords = stopwords.concat(adult);
+    var stopwords = stopwords.concat(useless);
+
+    var queue = [document.body];
+    var curr = -1;
+
+    while (curr = queue.pop()) {
+        for(var i = 0; i < stopwords.length; ++i) {
+            console.log(stopwords[i]);
+            if (curr.textContent.toLowerCase().match(stopwords[i])) {
+                console.log(curr);
+                return stopwords[i];
+            }
+        }
+    }
+
+    return ""; // Found nothing
+}
+
+WAIT_TIME = 10; // Default wait time is 10 seconds
+EXTRA_TEXT = ""; // Nothing by default
+
+var found_word_in_text = find_harmfull_content();
+if( found_word_in_text ) {
+  WAIT_TIME = WAIT_TIME * 10;
+  EXTRA_TEXT = "<br /> <br/> EXTREMELY HARMFUL CONTENT DETECTED: " + found_word_in_text;
+}
+
 function hidepage() {
   var div_elem = document.createElement('div');
-  var style = 'position: fixed; width: 95%; height: 95%; opacity: 0.98; z-index: 10000; ';
-  style = style + 'background: linear-gradient(to bottom right, black, orange); ';
+  var style = 'position: fixed; width: 95%; height: 95%; opacity: 0.95; z-index: 10000; ';
+  style = style + 'background: repeating-linear-gradient( 45deg, transparent, transparent 40px, black 40px, orange 80px),';
+  style = style + ' linear-gradient(to bottom right, black, orange);';
   style = style + '-moz-box-shadow: 3px 3px 5px 6px grey; ';
   style = style + '-webkit-box-shadow: 3px 3px 5px 6px grey; ';
   style = style + 'box-shadow: 3px 3px 5px 6px #ccc; ';
@@ -14,7 +57,7 @@ function hidepage() {
 
   var func = "function RMthisElement(){ document.getElementById('random_IO0_3279').remove(); }";
   var func_call = "(function() { " +
-      "var time = 10, " +
+      "var time = " + WAIT_TIME + ", " +
           "delta = 1, " +
           "tid; " +
 
@@ -42,6 +85,7 @@ function hidepage() {
   text = "Wait and relax <span id='random_j25893567fwajA'>10</span> seconds. <br /> "
   text = text + "This tab must be visible or the counter stops. <br /> <br /> "
   text = text + "Take a deep breath."
+  text = text + EXTRA_TEXT
   center_div.innerHTML = text
 
   div_elem.appendChild(center_div);
